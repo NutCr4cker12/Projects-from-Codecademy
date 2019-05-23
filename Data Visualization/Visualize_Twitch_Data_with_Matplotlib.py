9f17abab4a9cba7223c2b8bb7f3fc89d34c8aa89
NutCr4cker12 <44898642+NutCr4cker12@users.noreply.github.com>
2019-05-23 15:20:50 +0300

    Update Visualize_Twitch_Data_with_Matplotlib.py

--- Data Visualization/Visualize_Twitch_Data_with_Matplotlib.py
+++ Data Visualization/Visualize_Twitch_Data_with_Matplotlib.py
@@ -1,4 +1,3 @@
-import codecademylib3_seaborn
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
@@ -7,15 +6,13 @@ games = ["LoL", "Dota 2", "CS:GO", "DayZ", "HOS", "Isaac", "Shows", "Hearth", "W
 viewers =  [1070, 472, 302, 239, 210, 171, 170, 90, 86, 71]
 
 # Bar Graph: Featured Games
+ax = plt.subplot(1, 3, 1)
 plt.bar(range(len(games)), viewers, color='slateblue')
 plt.legend(["Twitch"])
 plt.xlabel('Games')
 plt.ylabel('Viewers')
-ax = plt.subplot()
 ax.set_xticks(range(0, 10))
 ax.set_xticklabels(games, rotation=30)
-plt.show()
-plt.close()
 
 # Pie Chart: League of Legends Viewers' Whereabouts
 labels = ["US", "DE", "CA", "N/A", "GB", "TR", "BR", "DK", "PL", "BE", "NL", "Others"]
@@ -23,28 +20,25 @@ countries = [447, 66, 64, 49, 45, 28, 25, 20, 19, 17, 17, 279]
 colors = ['lightskyblue', 'gold', 'lightcoral', 'gainsboro', 'royalblue', 'lightpink', 'darkseagreen', 'sienna', 'khaki', 'gold', 'violet', 'yellowgreen']
 explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
+plt.subplot(1, 3, 2)
 plt.pie(countries, explode=explode, colors=colors, shadow=True, startangle=345, autopct='%1.0f%%', pctdistance=1.15)
 plt.title("League of Legends Viewers' Whereabouts")
 plt.legend(labels, loc="right")
-plt.show()
-plt.close()
 
 # Line Graph: Time Series Analysis
 
 hour = range(24)
 
 viewers_hour = [30, 17, 34, 29, 19, 14, 3, 2, 4, 9, 5, 48, 62, 58, 40, 51, 69, 55, 76, 81, 102, 120, 71, 63]
-
+ax_2 = plt.subplot(1, 3, 3)
 plt.title("Time Series")
 plt.xlabel("Hour")
 plt.ylabel("Viewers")
 plt.plot(hour, viewers_hour)
 plt.legend(['2015-01-01'])
-ax = plt.subplot()
-ax.set_xticks(hour)
-ax.set_yticks([0, 20, 40, 60, 80, 100, 120, 140])
+ax_2.set_xticks(hour)
+ax_2.set_yticks([0, 20, 40, 60, 80, 100, 120, 140])
 y_upper = [i + (i*0.15) for i in viewers_hour]
 y_lower = [i - (i*0.15) for i in viewers_hour]
 plt.fill_between(hour, y_lower, y_upper, alpha=0.2)
 plt.show()
-plt.close()
